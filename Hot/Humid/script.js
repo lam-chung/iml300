14767e7ebb7e6511397ea2721a9224aa6c9931b7
lam-chung <lnchung@usc.edu>
2025-12-02 17:28:05 -0800

    new

---
+++ Hot/Humid/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+  $(".character-image").each(function () {
+    const originalHeight = $(this).height();
+
+    $(this).hover(
+      function () {
+        $(this).stop().animate({ height: originalHeight + 30 }, 200);
+      },
+      function () {
+        $(this).stop().animate({ height: originalHeight }, 200);
+      }
+    );
+  });
+
+});
+
